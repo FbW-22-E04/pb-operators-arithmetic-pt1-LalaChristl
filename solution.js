187174025990d515ad0d21099b65d3a8f34141d5
LalaChristl <lesleyannchristl@gmail.com>
2022-06-30 15:27:25 +0200

    added solution.js file

--- solution.js
+++ solution.js
@@ -1,1 +1,58 @@
 // write your code in this file
+
+//Question 1 
+let total = 6450 / 15;
+console.log('Q1:', total);
+
+// Question 2
+const totalIncome = 500 * 52;
+console.log('Q2:',totalIncome);
+
+// Question 3
+console.log('Q3:', 0.17 * 30); 
+
+// Question 4
+console.log('Q4:', 4.75 * 4, 'cm');
+
+// Question 5
+console.log('Q5:', 5 + 6 + 7, 'cm');
+
+// Question 6
+console.log(`Q6: ${5 * 4} cm^2`); 
+
+// Question 7
+console.log(`Q7: ${(3 * 4) / 2} cm`)
+
+// Question 8
+console.log(`Q8: ${9 * 9 * 9} cm`)
+
+// Question 9
+console.log(`Q9: ${22.35 * 1.1} ${26.67 * 1.15} ${35.92 * 1.20} euros`)
+
+// Question 10 
+console.log(`Q10: ${(8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4) / 10} avg. hours worked per day`)
+
+// Question 11
+console.log(`Q11: Score in the sixth test: -- ${85 * 6 - (75 + 70 + 85 + 90 + 100)}`)
+
+// Question 12 
+console.log(`Q12: James needs a minimun of -- ${80 * 9 - 78 * 8} % to get an 80% average`)
+
+/**
+ * 80
+ * 9
+ * 
+ * 8
+ * 78
+ * let y be the sum of the first 8 scores
+ * y/8 = 78
+ * y = 78*8 = the sum of the first 8 tests
+ * 
+ * let x be the minimum James needs
+ * (78*8 + x) / 9 = 80
+ * x = 80*9-(78*8)
+ */
+
+
+
+
